991a0e5ada1cd97ff0d02a6e45feee671605d3b1
Emre Ergun <emreergun@engramsoft.com>
2024-10-20 18:34:17 +0300

    background colors and gradient changed

--- src/components/HeroBg.tsx
+++ src/components/HeroBg.tsx
@@ -2,7 +2,7 @@ export default function HeroBg() {
   return (
     <div>
       <div className='absolute top-0 -z-10 h-full w-full'>
-        <div className='fixed bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] dark:bg-[rgba(121,55,102,0.5)] opacity-60 blur-[80px]'></div>
+        <div className='fixed bottom-auto left-[50%] -translate-x-[50%] -top-1/3 h-[1024px] w-[1024px]  rounded-full bg-[rgba(173,109,244,0.5)] dark:bg-[rgba(121,55,102,0.5)] opacity-60 blur-[80px]'></div>
       </div>
     </div>
   );
