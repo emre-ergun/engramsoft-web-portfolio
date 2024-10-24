52f3c608c0841c022691fdee4a48be4ada3682e6
Emre Ergun <emreergun@engramsoft.com>
2024-10-24 18:45:01 +0300

    min-width added to icons in buttons

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -50,21 +50,21 @@ export default function Hero() {
             <a
               href='/EmreErgunCV01072024.pdf'
               download
-              className='group bg-white px-5 py-1 flex w-1/2 items-center justify-around gap-2 rounded-xl borderBlack outline-none hover:scale-105 active:scale-100  transition dark:bg-white/10 text-nowrap'
+              className='group bg-white px-5 py-1 flex w-1/2 items-center justify-center gap-2 rounded-xl borderBlack outline-none hover:scale-105 active:scale-100  transition dark:bg-white/10 text-nowrap'
             >
               Download CV{' '}
-              <HiDownload className='opacity-70 group-hover:opacity-100 group-hover:translate-x-1 transition' />
+              <HiDownload className='opacity-70 min-w-6 group-hover:opacity-100 group-hover:translate-x-1 transition' />
             </a>
             <a
               href='#contact'
-              className='group bg-gray-900 text-white px-5 py-1 flex w-1/2 items-center justify-around gap-2 rounded-xl outline-none hover:scale-105 hover:bg-gray-950 active:scale-100 transition dark:bg-white dark:text-black text-nowrap'
+              className='group bg-gray-900 text-white px-5 py-1 flex w-1/2 items-center justify-center gap-2 rounded-xl outline-none hover:scale-105 hover:bg-gray-950 active:scale-100 transition dark:bg-white dark:text-black text-nowrap'
               onClick={() => {
                 setActiveSection('Contact');
                 setTimeOfLastClick(Date.now());
               }}
             >
               Contact me{' '}
-              <BsArrowRight className='opacity-70 group-hover:opacity-100 group-hover:translate-x-1 transition' />
+              <BsArrowRight className='opacity-70 min-w-6 group-hover:opacity-100 group-hover:translate-x-1 transition' />
             </a>
           </motion.div>
         </div>
