f8f14e7e957f9cdba00e8ef7b51ef87f025bda0b
Emre Ergun <emreergun@engramsoft.com>
2024-10-25 14:59:46 +0300

    projects section completed by creation project card, with mock data

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,5 +1,8 @@
+import { projectsData } from '../../lib/data';
 import { useSectionInView } from '../../lib/hooks';
+import Project from '../components/Project';
 import SectionHeader from '../components/SectionHeader';
+import React from 'react';
 
 const Projects = () => {
   const { ref } = useSectionInView('Projects');
@@ -10,7 +13,13 @@ const Projects = () => {
       className='mb-28 max-w-[50rem] text-center sm:mb-12 scroll-mt-28'
     >
       <SectionHeader title='My Projects' />
-      <div ref={ref} className='h-[1024px] w-32'></div>
+      <div ref={ref} className='flex flex-col'>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
     </section>
   );
 };
