0cf2405deb4db0ea4c4f74a283daaa0c842f7e09
Emre Ergun <emreergun@engramsoft.com>
2024-12-10 00:33:33 +0300

    new projects and skills added

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProfilePicture from '/profile.svg';
-import rmtdevImg from '/rmtdev.png';
-import wordanalyticsImg from '/wordanalytics.png';
+import adminDashboard from '/admin-dashboard.png';
+import mobileShop from '/mobile-shop.png';
 import portfolioSite from '/portfolio.png';
 import blogSite from '/blog.png';
 import { VscDebugConsole } from 'react-icons/vsc';
@@ -49,6 +49,7 @@ export const skillsData = [
   'Tailwind',
   'Nativewind',
   'React Context',
+  'Zustand',
   'Python',
   'Rust',
   'C/C++',
@@ -87,33 +88,63 @@ export const projectsData = [
     title: 'Portfolio Web Site',
     description:
       'I developed my own portfolio website. I used modern frameworks and focused on responsive design. It is automaticaly built and deployed to the cpanel.',
-    tags: ['React', 'Tailwind', 'TypeScript', 'Emailjs', 'Tailwind', 'Vite', 'Github CI/CD'],
+    tags: [
+      'React',
+      'Tailwind',
+      'TypeScript',
+      'Emailjs',
+      'Tailwind',
+      'Vite',
+      'Github CI/CD',
+    ],
     imageUrl: portfolioSite,
     url: 'https://portfolio.engramsoft.com',
   },
   {
     title: 'Blog Web Site',
     description:
-    'I developed a blog website. It is based on React and React Router. Supabase as a backend and TipTap for creating new posts. You can test editor on New Post page.',
-    tags: ['React', 'Tailwind', 'TypeScript', 'React Router', 'Supabase', 'TipTap', 'React Query'],
+      'I developed a blog website. It is based on React and React Router. Supabase as a backend and TipTap for creating new posts. You can test editor on New Post page.',
+    tags: [
+      'React',
+      'Tailwind',
+      'TypeScript',
+      'React Router',
+      'Supabase',
+      'TipTap',
+      'React Query',
+    ],
     imageUrl: blogSite,
     url: 'https://blog.engramsoft.com',
   },
   {
-    title: 'Lorem ipsum dolor',
+    title: 'Admin Dashboard',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut erat lobortis, ullamcorper velit eget, bibendum nibh. Duis scelerisque ipsum eu est eleifend venenatis.',
-    tags: ['React', 'TypeScript', 'Next.js', 'Tailwind', 'Redux'],
-    imageUrl: rmtdevImg,
-    url: 'https://engramsoft.com',
+      'I developed an Admin Dashboard to manage products, categories and orders. It is able to send notifications to mobile application. It is based on Next.js and Typescript. It is a video tuturial from freecodecamp.org',
+    tags: [
+      'Next.js',
+      'Typescript',
+      'shadcn/ui',
+      'Framer',
+      'Vercel',
+      'supabase',
+    ],
+    imageUrl: adminDashboard,
+    url: 'https://engramsoft-shop-admin.vercel.app/admin',
   },
   {
-    title: 'Lorem ipsum dolor',
+    title: 'Mobile App - Shopping',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut erat lobortis, ullamcorper velit eget, bibendum nibh. Duis scelerisque ipsum eu est eleifend venenatis.',
-    tags: ['React', 'Next.js', 'SQL', 'Tailwind', 'Framer'],
-    imageUrl: wordanalyticsImg,
-    url: 'https://engramsoft.com',
+      'I developed a mobile applicaiton for online shopping. It is based on React Native and Expo. It is a video tuturial from freecodecamp.org',
+    tags: [
+      'React Native',
+      'expo',
+      'Typescript',
+      'zustand',
+      'supabase',
+      'stripe',
+    ],
+    imageUrl: mobileShop,
+    url: 'https://github.com/emre-ergun/engramsoft-rn-shop',
   },
 ] as const;
 
